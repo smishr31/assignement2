52b4d87d26836a0e528fff38fa91cd5462166767
smishr31 <sajalmishra910@gmail.com>
2025-05-18 22:56:20 +0530

    Add files via upload

---
+++ Assignement2_Task_2.py
@@ -0,0 +1,4 @@
+c=0
+for i in range(1,50):
+    c = c + i
+print("The Sum of integer from 1 to 50 : ",c)
---
+++ assignement2_Task_1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter  number: "))
+
+if (a%2 == 0):
+    print(f"{a} is an Even Number")
+else:
+    print(f"{a} is an odd number")
